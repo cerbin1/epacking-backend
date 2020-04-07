26783b41b522ded5b8c53bbd6d275f21eb6a46a1
cerbin1 <gekarez@gmail.com>
2020-04-07 14:14:47 +0200

    Initial commit

---
+++ src/main/java/com/epacking/TestController.java
@@ -0,0 +1,13 @@
+package com.epacking;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class TestController {
+
+    @GetMapping("/test")
+    public String get() {
+        return "test-string";
+    }
+}
---
+++ src/test/java/com/epacking/EpackingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.epacking;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EpackingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
